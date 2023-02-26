2fcd8f55452960f30d1961c577fa4220c2749c75
Mykhailo Babiuk <master7dok@gmail.com>
2023-02-26 18:37:06 +0100

    added admin list

---
+++ src/main/java/com/mbdev/controllers/AdminController.java
@@ -0,0 +1,35 @@
+package com.mbdev.controllers;
+
+import com.mbdev.dao.PersonDAO;
+import com.mbdev.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public AdminController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping()
+    public String makeAdminPage(Model model, @ModelAttribute("person")Person person){
+        model.addAttribute("people", personDAO.index());
+
+        return "adminPage";
+    }
+    @PatchMapping("/add")
+    public String makeAdmin(@ModelAttribute("person") Person person) {
+        System.out.println(person.getId());
+
+        return "redirect:/people";
+    }
+}
